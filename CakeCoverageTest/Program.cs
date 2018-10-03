6cbe434b04dcccb36ac765f650893053a0c289f8
Lee Richardson <github@leerichardson.com>
2018-10-02 22:11:31 -0400

    Add project files.

---
+++ CakeCoverageTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CakeCoverageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
